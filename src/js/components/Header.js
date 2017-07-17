2769cf4c40ea45fa60dca1a91de88144fff600e2
Dario Breitenstein <chdabre@users.noreply.github.com>
2017-07-17 18:20:46 +0200

    initial cleanup

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,18 +1,10 @@
 import React from "react";
 
-import Title from "./Header/Title";
-
 export default class Header extends React.Component {
-  handleChange(e) {
-    const title = e.target.value;
-    this.props.changeTitle(title);
-  }
-
   render() {
     return (
       <div>
-        <Title title={this.props.title} />
-        <input value={this.props.title} onChange={this.handleChange.bind(this)} />
+        <h1> Hello! </h1>
       </div>
     );
   }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "AdventureRooms",
+
     };
   }
 
@@ -18,7 +17,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+        <Header />
       </div>
     );
   }
